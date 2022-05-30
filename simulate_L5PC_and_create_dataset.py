972545f8ab0afe449a4ecba8ee60ada05e17cc98
Sungho Hong <shhong@oist.jp>
2022-05-30 13:24:13 +0900

    remove importing cPickle

--- simulate_L5PC_and_create_dataset.py
+++ simulate_L5PC_and_create_dataset.py
@@ -2,7 +2,7 @@ import os
 import sys
 import numpy as np
 from scipy import signal
-import cPickle as pickle
+import pickle
 import time
 import neuron
 from neuron import h
@@ -100,84 +100,84 @@ def generate_input_spike_trains_for_simulation(sim_duration_ms, basal_seg_length
                                                num_bas_ex_spikes_per_100ms_range, num_apic_ex_spikes_per_100ms_range,
                                                num_bas_ex_inh_spike_diff_per_100ms_range, num_apic_ex_inh_spike_diff_per_100ms_range,
                                                inst_rate_sampling_time_interval_options_ms, temporal_inst_rate_smoothing_sigma_options_ms):
-    
+
     # extract the number of basal and apical segments
     num_basal_segments  = len(basal_seg_length_um)
     num_apical_segments = len(apical_seg_length_um)
-        
+
     # adjust segment lengths (with "min_seg_length_um")
     adjusted_basal_length_um  = min_seg_length_um + basal_seg_length_um
     adjusted_apical_length_um = min_seg_length_um + apical_seg_length_um
-    
+
     # calc sum of seg length (to be used for normalization later on)
     total_adjusted_basal_tree_length_um  = adjusted_basal_length_um.sum()
     total_adjusted_apical_tree_length_um = adjusted_apical_length_um.sum()
-    
+
     # randomly sample inst rate (with some uniform noise) smoothing sigma
     keep_inst_rate_const_for_ms = inst_rate_sampling_time_interval_options_ms[np.random.randint(len(inst_rate_sampling_time_interval_options_ms))]
     keep_inst_rate_const_for_ms += int(2 * inst_rate_sampling_time_interval_jitter_range * np.random.rand() - inst_rate_sampling_time_interval_jitter_range)
-    
+
     # randomly sample smoothing sigma (with some uniform noise)
     temporal_inst_rate_smoothing_sigma = temporal_inst_rate_smoothing_sigma_options_ms[np.random.randint(len(temporal_inst_rate_smoothing_sigma_options_ms))]
     temporal_inst_rate_smoothing_sigma += int(2 * temporal_inst_rate_smoothing_sigma_jitter_range * np.random.rand() - temporal_inst_rate_smoothing_sigma_jitter_range)
-    
+
     num_inst_rate_samples = int(np.ceil(float(sim_duration_ms) / keep_inst_rate_const_for_ms))
-    
+
     # create the coarse inst rates with units of "total spikes per tree per 100 ms"
     num_bas_ex_spikes_per_100ms   = np.random.uniform(low=num_bas_ex_spikes_per_100ms_range[0], high=num_bas_ex_spikes_per_100ms_range[1], size=(1,num_inst_rate_samples))
     num_bas_inh_spikes_low_range  = np.maximum(0, num_bas_ex_spikes_per_100ms + num_bas_ex_inh_spike_diff_per_100ms_range[0])
     num_bas_inh_spikes_high_range = num_bas_ex_spikes_per_100ms + num_bas_ex_inh_spike_diff_per_100ms_range[1]
     num_bas_inh_spikes_per_100ms  = np.random.uniform(low=num_bas_inh_spikes_low_range, high=num_bas_inh_spikes_high_range, size=(1,num_inst_rate_samples))
-    
+
     num_apic_ex_spikes_per_100ms   = np.random.uniform(low=num_apic_ex_spikes_per_100ms_range[0], high=num_apic_ex_spikes_per_100ms_range[1],size=(1,num_inst_rate_samples))
     num_apic_inh_spikes_low_range  = np.maximum(0, num_apic_ex_spikes_per_100ms + num_apic_ex_inh_spike_diff_per_100ms_range[0])
     num_apic_inh_spikes_high_range = num_apic_ex_spikes_per_100ms + num_apic_ex_inh_spike_diff_per_100ms_range[1]
     num_apic_inh_spikes_per_100ms  = np.random.uniform(low=num_apic_inh_spikes_low_range, high=num_apic_inh_spikes_high_range, size=(1,num_inst_rate_samples))
-    
+
     # convert to units of "per_1um_per_1ms"
     ex_bas_spike_rate_per_1um_per_1ms   = num_bas_ex_spikes_per_100ms   / (total_adjusted_basal_tree_length_um  * 100.0)
     inh_bas_spike_rate_per_1um_per_1ms  = num_bas_inh_spikes_per_100ms  / (total_adjusted_basal_tree_length_um  * 100.0)
     ex_apic_spike_rate_per_1um_per_1ms  = num_apic_ex_spikes_per_100ms  / (total_adjusted_apical_tree_length_um * 100.0)
     inh_apic_spike_rate_per_1um_per_1ms = num_apic_inh_spikes_per_100ms / (total_adjusted_apical_tree_length_um * 100.0)
-            
+
     # kron by space (uniform distribution across branches per tree)
     ex_bas_spike_rate_per_seg_per_1ms   = np.kron(ex_bas_spike_rate_per_1um_per_1ms  , np.ones((num_basal_segments,1)))
     inh_bas_spike_rate_per_seg_per_1ms  = np.kron(inh_bas_spike_rate_per_1um_per_1ms , np.ones((num_basal_segments,1)))
     ex_apic_spike_rate_per_seg_per_1ms  = np.kron(ex_apic_spike_rate_per_1um_per_1ms , np.ones((num_apical_segments,1)))
     inh_apic_spike_rate_per_seg_per_1ms = np.kron(inh_apic_spike_rate_per_1um_per_1ms, np.ones((num_apical_segments,1)))
-        
+
     # vstack basal and apical
     ex_spike_rate_per_seg_per_1ms  = np.vstack((ex_bas_spike_rate_per_seg_per_1ms , ex_apic_spike_rate_per_seg_per_1ms))
     inh_spike_rate_per_seg_per_1ms = np.vstack((inh_bas_spike_rate_per_seg_per_1ms, inh_apic_spike_rate_per_seg_per_1ms))
-    
+
     # add some spatial multiplicative randomness (that will be added to the sampling noise)
     ex_spike_rate_per_seg_per_1ms  = np.random.uniform(low=0.5, high=1.5, size=ex_spike_rate_per_seg_per_1ms.shape ) * ex_spike_rate_per_seg_per_1ms
     inh_spike_rate_per_seg_per_1ms = np.random.uniform(low=0.5, high=1.5, size=inh_spike_rate_per_seg_per_1ms.shape) * inh_spike_rate_per_seg_per_1ms
-    
+
     # concatenate the adjusted length
     adjusted_length_um = np.hstack((adjusted_basal_length_um, adjusted_apical_length_um))
-    
+
     # multiply each segment by it's length (now every segment will have firing rate proportional to it's length)
     ex_spike_rate_per_seg_per_1ms  = ex_spike_rate_per_seg_per_1ms  * np.tile(adjusted_length_um[:,np.newaxis], [1, ex_spike_rate_per_seg_per_1ms.shape[1]])
     inh_spike_rate_per_seg_per_1ms = inh_spike_rate_per_seg_per_1ms * np.tile(adjusted_length_um[:,np.newaxis], [1, inh_spike_rate_per_seg_per_1ms.shape[1]])
-        
+
     # kron by time (crop if there are leftovers in the end) to fill up the time to 1ms time bins
     ex_spike_rate_per_seg_per_1ms  = np.kron(ex_spike_rate_per_seg_per_1ms , np.ones((1,keep_inst_rate_const_for_ms)))[:,:sim_duration_ms]
     inh_spike_rate_per_seg_per_1ms = np.kron(inh_spike_rate_per_seg_per_1ms, np.ones((1,keep_inst_rate_const_for_ms)))[:,:sim_duration_ms]
-    
+
     # filter the inst rates according to smoothing sigma
     smoothing_window = signal.gaussian(1.0 + 7 * temporal_inst_rate_smoothing_sigma, std=temporal_inst_rate_smoothing_sigma)[np.newaxis,:]
     smoothing_window /= smoothing_window.sum()
     seg_inst_rate_ex_smoothed  = signal.convolve(ex_spike_rate_per_seg_per_1ms,  smoothing_window, mode='same')
     seg_inst_rate_inh_smoothed = signal.convolve(inh_spike_rate_per_seg_per_1ms, smoothing_window, mode='same')
-    
+
     # sample the instantanous spike prob and then sample the actual spikes
     ex_inst_spike_prob = np.random.exponential(scale=seg_inst_rate_ex_smoothed)
     ex_spikes_bin      = np.random.rand(ex_inst_spike_prob.shape[0], ex_inst_spike_prob.shape[1]) < ex_inst_spike_prob
-    
+
     inh_inst_spike_prob = np.random.exponential(scale=seg_inst_rate_inh_smoothed)
     inh_spikes_bin      = np.random.rand(inh_inst_spike_prob.shape[0], inh_inst_spike_prob.shape[1]) < inh_inst_spike_prob
-    
+
     return ex_spikes_bin, inh_spikes_bin
 
 
@@ -191,27 +191,27 @@ def GetDirNameAndFileName(numOutputSpikes, randomSeed):
         dendritesKind = 'passiveDendrites'
     else:
         dendritesKind += '_Ih_vshift_%d_SKE2_mult_%d' %(Ih_vshift, 100 * SKE2_mult_factor)
-        
+
     modelString = cellType + '__' + dendritesKind + '__' + synapseTypes
     dirToSaveIn = resultsSavedIn_rootFolder + modelString + '//'
-    
+
     # string to describe input
     string1 = 'exBas_%d_%d_inhBasDiff_%d_%d' %(num_bas_ex_spikes_per_100ms_range[0],num_bas_ex_spikes_per_100ms_range[1],
                                                num_bas_ex_inh_spike_diff_per_100ms_range[0],num_bas_ex_inh_spike_diff_per_100ms_range[1])
     string2 = 'exApic_%d_%d_inhApicDiff_%d_%d' %(num_apic_ex_spikes_per_100ms_range[0],num_apic_ex_spikes_per_100ms_range[1],
                                                  num_apic_ex_inh_spike_diff_per_100ms_range[0],num_apic_ex_inh_spike_diff_per_100ms_range[1])
     inputString = string1 + '__' + string2
-    
+
     # string to describe simulation
     savedDVTs = ''
     if collectAndSaveDVTs:
         savedDVTs = 'DVTs'
-    
+
     string3 = 'saved_InputSpikes_%s__%d_outSpikes__%d_simulationRuns__%d_secDuration__randomSeed_%d'
     simulationString = string3 %(savedDVTs, numOutputSpikes, numSimulations,totalSimDurationInSec, randomSeed)
-                  
+
     filenameToSave = inputString + '__' + simulationString + '.p'
-                 
+
     return dirToSaveIn, filenameToSave
 
 
@@ -319,10 +319,10 @@ def ConnectEmptyEventGenerator(synapse):
 def CreateCombinedColorImage(dendriticVoltageTraces, excitatoryInputSpikes, inhibitoryInputSpikes):
     minV = -85
     maxV = 35
-    
+
     excitatoryInputSpikes = signal.fftconvolve(excitatoryInputSpikes, np.ones((3,3)), mode='same')
     inhibitoryInputSpikes = signal.fftconvolve(inhibitoryInputSpikes, np.ones((3,3)), mode='same')
-    
+
     stimulationImage = np.zeros((np.shape(excitatoryInputSpikes)[0],np.shape(excitatoryInputSpikes)[1],3))
     stimulationImage[:,:,0] = 0.98 * (dendriticVoltageTraces - minV) / (maxV - minV) + inhibitoryInputSpikes
     stimulationImage[:,:,1] = 0.98 * (dendriticVoltageTraces - minV) / (maxV - minV) + excitatoryInputSpikes
@@ -456,7 +456,7 @@ for simInd in range(numSimulations):
 
     inputSpikeTrains_ex  = ex_spikes_bin
     inputSpikeTrains_inh = inh_spikes_bin
-        
+
     ## convert binary vectors to dict of spike times for each seg ind
     exSpikeSegInds, exSpikeTimes = np.nonzero(inputSpikeTrains_ex)
     exSpikeTimesMap = {}
@@ -465,7 +465,7 @@ for simInd in range(numSimulations):
             exSpikeTimesMap[segInd].append(synTime)
         else:
             exSpikeTimesMap[segInd] = [synTime]
-    
+
     inhSpikeSegInds, inhSpikeTimes = np.nonzero(inputSpikeTrains_inh)
     inhSpikeTimesMap = {}
     for segInd, synTime in zip(inhSpikeSegInds,inhSpikeTimes):
@@ -473,21 +473,21 @@ for simInd in range(numSimulations):
             inhSpikeTimesMap[segInd].append(synTime)
         else:
             inhSpikeTimesMap[segInd] = [synTime]
-    
-    
+
+
     ## run simulation ########################
     allExNetCons = []
     allExNetConEventLists = []
-    
+
     allInhNetCons = []
     allInhNetConEventLists = []
-    
+
     allExSynapses = []
     allInhSynapses = []
-    
+
     for segInd, segment in enumerate(allSegments):
         ###### excitation ######
-    
+
         # define synapse and connect it to a segment
         if excitatorySynapseType == 'AMPA':
             exSynapse = DefineSynapse_AMPA(segment)
@@ -496,21 +496,21 @@ for simInd in range(numSimulations):
         else:
             assert False, 'Not supported Excitatory Synapse Type'
         allExSynapses.append(exSynapse)
-    
+
         # connect synapse
         netConnection = h.NetCon(None,exSynapse)
         netConnection.delay = 0
         netConnection.weight[0] = 1
-    
+
         # update lists
         allExNetCons.append(netConnection)
         if segInd in exSpikeTimesMap.keys():
             allExNetConEventLists.append(exSpikeTimesMap[segInd])
         else:
             allExNetConEventLists.append([])
-            
+
         ###### inhibition ######
-    
+
         # define synapse and connect it to a segment
         if inhibitorySynapseType == 'GABA_A':
             inhSynapse = DefineSynapse_GABA_A(segment)
@@ -521,17 +521,17 @@ for simInd in range(numSimulations):
         else:
             assert False, 'Not supported Inhibitory Synapse Type'
         allInhSynapses.append(inhSynapse)
-    
+
         # connect synapse
         netConnection = ConnectEmptyEventGenerator(inhSynapse)
-    
+
         # update lists
         allInhNetCons.append(netConnection)
         if segInd in inhSpikeTimesMap.keys():
             allInhNetConEventLists.append(inhSpikeTimesMap[segInd])
         else:
             allInhNetConEventLists.append([])  # insert empty list if no event
-    
+
     # define function to be run at the begining of the simulation to add synaptic events
     def AddAllSynapticEvents():
         for exNetCon, eventsList in zip(allExNetCons,allExNetConEventLists):
@@ -542,20 +542,20 @@ for simInd in range(numSimulations):
                 inhNetCon.event(eventTime)
 
     # add voltage and time recordings
-                
+
     # record time
     recTime = h.Vector()
     recTime.record(h._ref_t)
-    
+
     # record soma voltage
     recVoltageSoma = h.Vector()
     recVoltageSoma.record(L5PC.soma[0](0.5)._ref_v)
-    
+
     # record nexus voltage
     nexusSectionInd = 50
     recVoltageNexus = h.Vector()
     recVoltageNexus.record(L5PC.apic[nexusSectionInd](0.9)._ref_v)
-    
+
     # record all segments voltage
     if collectAndSaveDVTs:
         recVoltage_allSegments = []
@@ -563,7 +563,7 @@ for simInd in range(numSimulations):
             voltageRecSegment = h.Vector()
             voltageRecSegment.record(segment._ref_v)
             recVoltage_allSegments.append(voltageRecSegment)
-        
+
     preparationDurationInSeconds = time.time() - preparationStartTime
     print("preparing for single simulation took %.4f seconds" % (preparationDurationInSeconds))
 
@@ -580,11 +580,11 @@ for simInd in range(numSimulations):
     ## extract the params from the simulation
     # collect all relevent recoding vectors (input spike times, dendritic voltage traces, soma voltage trace)
     collectionStartTime = time.time()
-        
+
     origRecordingTime = np.array(recTime.to_python())
     origSomaVoltage   = np.array(recVoltageSoma.to_python())
     origNexusVoltage  = np.array(recVoltageNexus.to_python())
-    
+
     # high res - origNumSamplesPerMS per ms
     recordingTimeHighRes = np.arange(0, totalSimDurationInMS, 1.0 / numSamplesPerMS_HighRes)
     somaVoltageHighRes   = np.interp(recordingTimeHighRes, origRecordingTime, origSomaVoltage)
@@ -594,7 +594,7 @@ for simInd in range(numSimulations):
     recordingTimeLowRes = np.arange(0,totalSimDurationInMS)
     somaVoltageLowRes   = np.interp(recordingTimeLowRes, origRecordingTime, origSomaVoltage)
     nexusVoltageLowRes  = np.interp(recordingTimeLowRes, origRecordingTime, origNexusVoltage)
-    
+
     if collectAndSaveDVTs:
         dendriticVoltages = np.zeros((len(recVoltage_allSegments),recordingTimeLowRes.shape[0]))
         for segInd, recVoltageSeg in enumerate(recVoltage_allSegments):
@@ -605,15 +605,15 @@ for simInd in range(numSimulations):
     fallingAfter = np.hstack((somaVoltageHighRes[1:] - somaVoltageHighRes[:-1], 0)) < 0
     localMaximum = np.logical_and(fallingAfter, risingBefore)
     largerThanThresh = somaVoltageHighRes > -25
-    
+
     binarySpikeVector = np.logical_and(localMaximum,largerThanThresh)
     spikeInds = np.nonzero(binarySpikeVector)
     outputSpikeTimes = recordingTimeHighRes[spikeInds]
-    
+
     currSimulationResultsDict['recordingTimeHighRes'] = recordingTimeHighRes.astype(np.float32)
     currSimulationResultsDict['somaVoltageHighRes']   = somaVoltageHighRes.astype(np.float16)
     currSimulationResultsDict['nexusVoltageHighRes']  = nexusVoltageHighRes.astype(np.float16)
-    
+
     currSimulationResultsDict['recordingTimeLowRes'] = recordingTimeLowRes.astype(np.float32)
     currSimulationResultsDict['somaVoltageLowRes']   = somaVoltageLowRes.astype(np.float16)
     currSimulationResultsDict['nexusVoltageLowRes']  = nexusVoltageLowRes.astype(np.float16)
@@ -621,19 +621,19 @@ for simInd in range(numSimulations):
     currSimulationResultsDict['exInputSpikeTimes']  = exSpikeTimesMap
     currSimulationResultsDict['inhInputSpikeTimes'] = inhSpikeTimesMap
     currSimulationResultsDict['outputSpikeTimes']   = outputSpikeTimes.astype(np.float16)
-    
+
     if collectAndSaveDVTs:
         currSimulationResultsDict['dendriticVoltagesLowRes'] = dendriticVoltages.astype(np.float16)
-        
+
     numOutputSpikes = len(outputSpikeTimes)
     numOutputSpikesPerSim.append(numOutputSpikes)
     listOfISIs += list(np.diff(outputSpikeTimes))
-    
+
     listOfSingleSimulationDicts.append(currSimulationResultsDict)
-    
+
     dataCollectionDurationInSeconds = (time.time() - collectionStartTime)
     print("data collection per single simulation took %.4f seconds" % (dataCollectionDurationInSeconds))
-    
+
     entireSimulationDurationInMinutes = (time.time() - preparationStartTime) / 60
     print('-----------------------------------------------------------')
     print('finished simulation %d: num output spikes = %d' %(simInd + 1, numOutputSpikes))
@@ -647,24 +647,24 @@ for simInd in range(numSimulations):
         plt.figure(figsize=(30,15))
         plt.subplot(2,1,1); plt.title('input spike trains')
         plt.imshow(CreateCombinedColorImage(dendriticVoltages, inputSpikeTrains_ex, inputSpikeTrains_inh))
-        
+
         plt.subplot(2,1,2); plt.title('interpulated time - high res')
         plt.plot(recordingTimeHighRes, somaVoltageHighRes)
         plt.plot(recordingTimeHighRes, nexusVoltageHighRes)
         plt.xlim(0,totalSimDurationInMS)
         plt.ylabel('Voltage [mV]'); plt.legend(['soma','nexus'])
-    
+
         plt.figure(figsize=(30,15))
         plt.subplot(3,1,1); plt.title('dendritic voltage traces - low res')
         for segInd in range(len(recVoltage_allSegments)):
             plt.plot(recordingTimeLowRes, dendriticVoltages[segInd,:])
         plt.ylabel('Voltage [mV]')
-        
+
         plt.subplot(3,1,2); plt.title('interpulated time - low res')
         plt.plot(recordingTimeLowRes, somaVoltageLowRes)
         plt.plot(recordingTimeLowRes, nexusVoltageLowRes)
         plt.xlabel('time [msec]'); plt.ylabel('Voltage [mV]'); plt.legend(['soma','nexus','soma LowRes','nexus LowRes'])
-        
+
         plt.subplot(3,1,3); plt.title('voltage histogram')
         plt.hist(somaVoltageHighRes.ravel() , normed=True, bins=200, color='b', alpha=0.7)
         plt.hist(nexusVoltageHighRes.ravel(), normed=True, bins=200, color='r', alpha=0.7)
@@ -678,7 +678,7 @@ totalNumSimulationSeconds = totalSimDurationInSec * numSimulations
 averageOutputFrequency = totalNumOutputSpikes / float(totalNumSimulationSeconds)
 ISICV = np.std(listOfISIs) / np.mean(listOfISIs)
 entireExperimentDurationInMinutes = (time.time() - experimentStartTime) / 60
-                            
+
 # calculate some collective meassures of the experiment
 print('-------------------------------------------------\\')
 print("entire experiment took %.2f minutes" % (entireExperimentDurationInMinutes))
